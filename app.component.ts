bab5537e482babd62ea8b80dcc33f4d94b0db62d
harsha97sahajan <83864653+harsha97sahajan@users.noreply.github.com>
2021-10-03 18:50:04 +0530

    Add files via upload

---
+++ app.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  
+
+menuList=[]
+constructort(){}
+
+
+
+
+
+  
+  ngOnInit() {
+
+  }
+  
+}  
